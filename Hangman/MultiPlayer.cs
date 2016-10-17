1e2ee227f621ceb0460152e7c092a5956d6eac35
SAKIB RAHMAN <srskb15@gmail.com>
2016-10-17 21:00:12 +0600

    Revert "Initial Commit"
    This reverts commit 0052722560b588fb4351ba0b2557130c6e093ec0.

--- Hangman/MultiPlayer.cs
+++ Hangman/MultiPlayer.cs
@@ -10,25 +10,27 @@ namespace Hangman
     {
         private string player1;
         private string player2;
-        private int setter = 0;
-
+        private int turnController;
+        private string turn;
         private int playerOneScore;
         private int playerTwoScore;
-
         private int tries;
-        private int rounds;
 
         private String word;
         private string guessWord;
+        private string w;
         private char[] chars;
         private char[] guessChars;
-    
+
+
         public MultiPlayer()
         {
             this.player1 = "Player 1";
             this.player2 = "Player 2";
+            this.turn = "playerOne";
             this.playerOneScore = 0;
             this.playerTwoScore = 0;
+            this.turnController = 0;
             this.tries = 7;
             this.guessWord = "";
         }
@@ -37,43 +39,51 @@ namespace Hangman
         {
             this.player1 = player1;
             this.player2 = player2;
+            this.turn = "playerOne";
             this.playerOneScore = 0;
             this.playerTwoScore = 0;
+            this.turnController = 0;
             this.tries = 7;
             this.guessWord = "";
-        }
-
-        public void initialize()
-        {
-            this.tries = 7;
-            this.guessWord = "";
-        }
-        public void initializeScore()
-        {
-            this.playerOneScore = 0;
-            this.playerTwoScore = 0;
-        }
-        public int getWordLength()
-        {
-            return word.Length;
         }
 
         public void printGameName()
         {
             Console.Clear();
-            Console.WriteLine();
             string str = "Hangman Game Multiplayer!!!";
             string under = "------------------------------------";
             Console.Clear();
-            Console.WriteLine();
+
             Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (str.Length / 2)) + "}", str));
             Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (under.Length / 2)) + "}", under));
             Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (under.Length / 2)) + "}", under));
         }
 
+        public int getWordLength()
+        {
+            return word.Length;
+        }
+
+        public void setTurn()
+        {
+            if(turnController%2==0)
+            {
+                turn= "playerOne";
+                turnController++;
+            }
+            else
+            {
+                turn = "playerTwo";
+                turnController++;
+            }
+            if (turn == "playerOne")
+                Console.WriteLine("{0} Give Word !!!",player1);
+            else
+                Console.WriteLine("{0} Guess the word !!!",player2);
+        }
+
         public void setString()
         {
-            Console.WriteLine();
             Console.Write("Enter a word : ");
             word = Console.ReadLine();
             for (int i = 0; i < word.Length; i++)
@@ -84,44 +94,6 @@ namespace Hangman
             guessChars = guessWord.ToCharArray();
         }
 
-        public void PrintQuestion()
-        {
-            if (setter == 0)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Player Name: {0}", this.player1);
-            }
-            else if (setter==1)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Player Name: {0}", this.player2);
-            }
-        }
-
-        public void PrintAnswer()
-        {
-            if (setter == 0)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Player Name: {0}", this.player2);
-            }
-            else if (setter == 1)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Player Name: {0}", this.player1);
-            }
-        }
-
-        public void gameInfo()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Tries: {0}",this.tries);
-            Console.WriteLine();
-            Console.WriteLine("Length: {0}", this.getWordLength());
-            Console.WriteLine();
-            Console.WriteLine("Used: {0}" , this.tries);
-        }
-        
         public void randomGenerator()
         {
             Random rnd = new Random(DateTime.Now.Ticks.GetHashCode());
@@ -131,131 +103,58 @@ namespace Hangman
 
         public void searchString(char s)
         {
-            int flag = 0;
             for(int i = 0; i < getWordLength(); i++)
             {
                 if (chars[i] == s)
                 {
                     guessChars[i]= s;
-                    flag = 1;
                 }
-                guessWord = new string(guessChars);
-                Console.WriteLine(guessWord);
-                Console.WriteLine(word);
             }
-            if(flag==0)
-            {
-                tries--;
-            }
+            guessWord = new string(guessChars);
+            Console.WriteLine(guessWord);
         }
 
-        public bool checkResult()
+        public void checkResult()
         {
-            if (word == guessWord && setter==1)
+            if (word == guessWord)
             {
-                playerOneScore++;
-                return true;
-            }
 
-            else if (word == guessWord && setter == 0)
-            {
-                playerTwoScore++;
-                return true;
             }
             else
             {
-                return false;
-            }
-                
+
+            }            
         }
+
 
         public void showResult()
         {
             Console.Clear();
             printGameName();
-
-            Console.WriteLine();
-            Console.WriteLine();
-            PrintAnswer();
             Console.WriteLine("Tries Left: " + tries);
-            Console.WriteLine();
-            Console.WriteLine(player1 + " : " + playerOneScore);
-            Console.WriteLine();
-            Console.WriteLine(player2 + " : " + playerTwoScore);
 
         }
 
         public void showFinalResult()
         {
-            printGameName();
 
-            Console.WriteLine();
-            Console.WriteLine("Toal Round : " + rounds);
-            Console.WriteLine();
-            Console.WriteLine(player1 + " Score : " + playerOneScore);
-            Console.WriteLine();
-            Console.WriteLine(player2 + " Score : " + playerTwoScore);
-
-            if (playerOneScore > playerTwoScore)
-                Console.WriteLine(player1 + " Win");
-            else if (playerOneScore < playerTwoScore)
-                Console.WriteLine(player2 + " Win");
-            else
-                Console.WriteLine("Draw");
         }
-
-        public void play()
-        {
-            Console.WriteLine(guessWord);
-            char s = Char.Parse(Console.ReadLine());
-            searchString(s);
-        }
-
         public void StartGame()
         {
-            initializeScore();
             printGameName();
+            randomGenerator();
+            setString();
+            searchString('i');
 
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.Write("How many Round you want to play ? : ");
-            rounds = Convert.ToInt32(Console.ReadLine());
-
-            int j;
-            for (j = 0; j < this.rounds; j++)
+            for (int i = 0; i < tries; i++)
             {
-                initialize();
-                printGameName();
-                Console.WriteLine();
-                Console.WriteLine("Round : " + (j + 1));
-                Console.WriteLine();
-                Console.WriteLine(player1 + " : " + playerOneScore);
-                Console.WriteLine();
-                Console.WriteLine(player2 + " : " + playerTwoScore);
-                PrintQuestion();
-                setString();
 
-                bool res = false;
-                while (tries!=0)
-                {
-                    printGameName();
-                    PrintAnswer();
-                    gameInfo();
-                    play();
-                    res=checkResult();
-                    if (res)
-                        break;
-                }
-
-                if ((tries == 0 && setter == 1) || (res && setter == 1))
-                    setter = 0;
-                else if (tries == 0 && setter == 0 || (res &&  setter == 0))
-                    setter = 1;
+                
             }
-            if(j==rounds)
-            {
-                showFinalResult();
-            }
+            
         }
+
+
+
     }
 }
